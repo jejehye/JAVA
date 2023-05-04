517143996d3689cd546193091c6397d25e0c324b
jeje <57799486+jejehye@users.noreply.github.com>
2023-05-04 10:18:56 +0900

    Create Map.java

---
+++ 자료형/Map.java
@@ -0,0 +1,25 @@
+1. Map 이란?
+
+: 맵은 key와 value를 한쌍으로 같는 자료형으로 리스트나 배열처러 순차적으로 해당 요소 값을 찾지않고 key를 통해서 value를 구한다. key 찾아내어 value를 구한다느 의미
+
+
+1-1) put 이란?
+   : key와 value는 put 메서드를 통해 추가 할수 있음
+   
+   예시) 
+   import java.util.*
+   
+   public class putSample {
+      public static void main(String args[]{
+      Map<String, String> map = new HashMap<>();
+      
+      }
+   
+   }
+                              
+ 1-2) get 이란?
+   : key에 해당하는 value값을 얻기 위해서는 get 메서드를 사용
+   
+   예시) 
+                          
+                              
